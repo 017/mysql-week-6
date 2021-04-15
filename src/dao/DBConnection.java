243e399c77d93b6aea99f5649fd90182af26a626
Robert Ochi <c4f117@gmail.com>
2021-04-15 17:02:06 -0600

    added DB connection config

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,5 +1,31 @@
 package dao;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	private final static String URL = "jdbc:mysql://localhost:3306/pet_db";
+	private final static String USERNAME = "root";
+	private final static String PASSWORD = "";
+	private static Connection connection;
+	private static DBConnection instance;
+	
+	private DBConnection(Connection connection) {
+		DBConnection.connection = connection;
+	}
+	
+	public static Connection getConnection() {
+		if (instance == null) {
+			try {
+				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				instance = new DBConnection(connection);
+				System.out.println("[MySQL DB] Connection Succeeded!");
+			} catch (SQLException e) {
+				System.out.println("[MySQL DB] Connection Failed!");
+				e.printStackTrace();
+			}
+		}
+		return DBConnection.connection;
+	}
 }
