1817d9a9cef6042d6b0f5d36f5fe70cf1b36b6d7
Robert Ochi <c4f117@gmail.com>
2021-04-15 18:22:14 -0600

    menu.java updated to begin implementing scanner and menu options

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -1,5 +1,85 @@
 package application;
 
-public class Menu {
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+import dao.PetDao;
+
+public class Menu {
+	private PetDao petDao;
+	private Scanner scanner = new Scanner(System.in);
+	private List<String> options = Arrays.asList(
+			"Display All Pets",
+			"Display Pets by Type",
+			"Display Pets by Breed",
+			"Display a Pet by ID",
+			"Create A Pet",
+			"Delete A Pet",
+			"Edit a Pet");
+	
+	public void start() {
+		String selection = "";
+		
+		do {
+			printMenu();
+			selection = scanner.nextLine();
+			try {
+
+				if (selection.equals("1")) {
+					displayallPets();
+				} else if (selection.equals("2")) {
+					displayPetsByType();
+				} else if (selection.equals("3")) {
+					displayPetsByBreed();
+				} else if (selection.equals("4")) {
+					displayPetByID();
+				} else if (selection.equals("5")) {
+					createPet();
+				} else if (selection.equals("6")) {
+					deletePet();
+				} else if (selection.equals("7")) {
+					editPet();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("Press enter to continue.");
+			scanner.nextLine();
+			
+		} while (!selection.equals("-1"));
+	}
+
+	private void deletePet() throws SQLException {
+		System.out.print("Enter Pet ID to Delete: ");
+		int id = Integer.parseInt(scanner.nextLine());
+		petDao.deletePetByID(id);
+	}
+
+	private void createPet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void displayPetByID() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void displayPetsByBreed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void displayPetsByType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void displayallPets() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/dao/PetDao.java
+++ src/dao/PetDao.java
@@ -1,5 +1,21 @@
 package dao;
 
+import java.sql.Connection;
+
 public class PetDao {
 
+	private Connection connection;
+	private final String GET_PETS_QUERY = "SELECT * FROM pets";
+	private final String GET_PET_BY_ID_QUERY = "SELECT * FROM pizzas WHERE pet_id = ?";
+	private final String DELETE_PET_BY_ID_QUERY = "DELETE FROM pizzas WHERE pet_id = ?";
+	
+	//TODO: make a SQL procedure for editing pets
+	private final String EDIT_PET_BY_ID_QUERY = "";
+	
+	//TODO: make a SQL procedure for entering new pets
+	private final String CREATE_NEW_PET_QUERY = "";
+	
+	public PetDao() {
+		connection = DBConnection.getConnection();
+	}
 }
